add22504afd0153670e3024316c11755221dbc2a
alsadoonmaryam <alsadoonmaryam47@gmail.com>
2025-11-14 19:20:23 -0500

    All test passing

--- Assignment3_Tests/Assignment3_Tests.cpp
+++ Assignment3_Tests/Assignment3_Tests.cpp
@@ -19,6 +19,23 @@ namespace RpsTests
             Assert::AreEqual("Player1", result);
         }
 
+        TEST_METHOD(Test_Paper_vs_Rock_Player1)
+        {
+            const char* result = playRockPaperScissors("Paper", "Rock");
+            Assert::AreEqual("Player1", result);
+        }
+
+        TEST_METHOD(Test_Scissors_vs_Scissors_Draw)
+        {
+            const char* result = playRockPaperScissors("Scissors", "Scissors");
+            Assert::AreEqual("Draw", result);
+        }
+
+        TEST_METHOD(Test_Rock_vs_Banana_Invalid)
+        {
+            const char* result = playRockPaperScissors("Rock", "Banana");
+            Assert::AreEqual("Invalid", result);
+        }
 
     };
 }
